c6e99132f5f5cb99f9b6da07414d581cb5b1ff6b
reynaldomarchell <reynaldomarchellba@gmail.com>
2023-07-19 14:52:37 +0700

    added bootstrap in login and register

--- login.php
+++ login.php
@@ -1,72 +1,92 @@
-<?php 
+<?php
 session_start();
 
-  include("connection.php");
-  include("functions.php");
+include("connection.php");
+include("functions.php");
 
-  if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $user_name = $_POST['user_name'];
-    $password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  $user_name = $_POST['user_name'];
+  $password = $_POST['password'];
 
-    if(!empty($user_name) && !empty($password) && !is_numeric($user_name)){
-      $query = "select * from users where user_name = '$user_name' limit 1";
-      $result = mysqli_query($con, $query);
+  if (!empty($user_name) && !empty($password) && !is_numeric($user_name)) {
+    $query = "select * from users where user_name = '$user_name' limit 1";
+    $result = mysqli_query($con, $query);
 
-      if($result){
-        if($result && mysqli_num_rows($result) > 0){
-          $user_data = mysqli_fetch_assoc($result);
+    if ($result) {
+      if ($result && mysqli_num_rows($result) > 0) {
+        $user_data = mysqli_fetch_assoc($result);
 
-          if($user_data['password'] === $password){
-            $_SESSION['name'] = $user_data['name'];
-            $_SESSION["user_name"] = $user_data["user_name"];
-            $user_name = $_SESSION["user_name"];
-            
-            $tasks_result = mysqli_query($con, "SELECT task FROM tasks WHERE user_name = '$user_name'");
-            while($row = mysqli_fetch_assoc($tasks_result)){
-              $_SESSION["TASK"][] = $row["task"];
-            }
-            mysqli_query($con, "DELETE FROM tasks WHERE user_name = '$user_name'");
+        if ($user_data['password'] === $password) {
+          $_SESSION['name'] = $user_data['name'];
+          $_SESSION["user_name"] = $user_data["user_name"];
+          $user_name = $_SESSION["user_name"];
 
-            $completed_result = mysqli_query($con, "SELECT task FROM completed WHERE user_name = '$user_name'");
-            while($row = mysqli_fetch_assoc($completed_result)){
-              $_SESSION["COMPLETED_TASK"][] = $row["task"];
-            }
-            mysqli_query($con, "DELETE FROM completed WHERE user_name = '$user_name'");
-            
-            header("Location: index.php");
-            die;
+          $tasks_result = mysqli_query($con, "SELECT task FROM tasks WHERE user_name = '$user_name'");
+          while ($row = mysqli_fetch_assoc($tasks_result)) {
+            $_SESSION["TASK"][] = $row["task"];
           }
+          mysqli_query($con, "DELETE FROM tasks WHERE user_name = '$user_name'");
+
+          $completed_result = mysqli_query($con, "SELECT task FROM completed WHERE user_name = '$user_name'");
+          while ($row = mysqli_fetch_assoc($completed_result)) {
+            $_SESSION["COMPLETED_TASK"][] = $row["task"];
+          }
+          mysqli_query($con, "DELETE FROM completed WHERE user_name = '$user_name'");
+
+          header("Location: index.php");
+          die;
         }
       }
-      echo "Wrong username or password!";
-    
-    } else{
-      echo "Wrong username or password!";
     }
+    echo "Wrong username or password!";
+
+  } else {
+    echo "Wrong username or password!";
   }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
   <title>Login</title>
 </head>
+
 <body>
-  <h1>Login</h1>
+  <div class="login template d-flex justify-content-center align-items-center vh-100 bg-primary">
+    <div class="form_container p-5 rounded bg-white">
+      <form method="POST">
+        <h3 class="text-center">Sign In</h3>
+        <div class="mb-2">
+          <label htmlFor="username">Enter your username</label>
+          <input type="text" placeholder="Username" class="form-control" name="user_name">
+        </div>
+        <div class="mb-2">
+          <label htmlFor="password">Enter your password</label>
+          <input type="password" placeholder="Password" class="form-control" name="password">
+        </div>
+        <!-- <div class="mb-2">
+          <input type="checkbox" class="custom-control custom-checkbox" id="check">
+          <label htmlFor="check" class="custom-input-label ms-2">Remember me</label>
+        </div> -->
+        <div class="d-grid mt-2">
+          <button class="btn btn-primary">Sign In</button>
+        </div>
+        <p class="text-end mt-2">
+          Don't have account? <a href="register.php" class="ms-2">Sign Up</a>
+        </p>
+      </form>
+    </div>
+  </div>
 
-  <form method="POST">
-    Enter your username<br>
-    <input type="text" name="user_name"><br><br>
-    Enter your password<br>
-    <input type="password" name="password"><br><br>
-    <input type="submit" value="Login"><br><br>
-  </form>
-
-  <a href="register.php">Click to register</a>
-
+  <script href="dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -1,48 +1,87 @@
-<?php 
+<?php
 session_start();
 
-  include "connection.php";
-  include "functions.php";
+include "connection.php";
+include "functions.php";
 
-  if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $name = $_POST["name"];
-    $user_name = $_POST['user_name'];
-    $password = $_POST['password'];
-    $confirm_password = $_POST["confirm_password"];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+  $name = $_POST["name"];
+  $user_name = $_POST['user_name'];
+  $password = $_POST['password'];
+  $confirm_password = $_POST["confirm_password"];
 
-    if(!empty($name) && !empty($user_name) && !empty($password) && !empty($confirm_password)
-    && !is_numeric($user_name) && $password == $confirm_password){
+  if (
+    !empty($name) && !empty($user_name) && !empty($password) && !empty($confirm_password)
+    && !is_numeric($user_name) && $password == $confirm_password
+  ) {
 
-      if((strlen($name) >= 1 && strlen($name) <= 20) && (strlen($user_name) >= 3 && strlen($user_name) <= 15)
-      && (strlen($password) >= 8 && strlen($password) <= 20)){
+    if (
+      (strlen($name) >= 1 && strlen($name) <= 20) && (strlen($user_name) >= 3 && strlen($user_name) <= 15)
+      && (strlen($password) >= 8 && strlen($password) <= 20)
+    ) {
 
-        $query = "insert into users (name,user_name,password) values ('$name','$user_name','$password')";
-  
-        mysqli_query($con, $query);
-  
-        header("Location: login.php");
-        die;
-      }else{
-        echo "Please enter with the valid length!";
-      }
+      $query = "insert into users (name,user_name,password) values ('$name','$user_name','$password')";
 
-    }else{
-      echo "Please enter some valid information!";
+      mysqli_query($con, $query);
+
+      header("Location: login.php");
+      die;
+    } else {
+      echo "Please enter with the valid length!";
     }
+
+  } else {
+    echo "Please enter some valid information!";
   }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
   <title>Register</title>
 </head>
+
 <body>
-  <h1>Register</h1>
+  <div class="signup template d-flex justify-content-center align-items-center vh-100 bg-primary">
+    <div class="form_container p-5 rounded bg-white">
+      <form method="POST">
+        <h3 class="text-center">Sign Up</h3>
+        <div class="mb-2">
+          <label htmlFor="name">Enter your name (1-20 character)</label>
+          <input type="text" placeholder="Name" class="form-control" name="name">
+        </div>
+        <div class="mb-2">
+          <label htmlFor="username">Enter your username (3-15 character)</label>
+          <input type="text" placeholder="Username" class="form-control" name="user_name">
+        </div>
+        <div class="mb-2">
+          <label htmlFor="password">Enter your password (8-20 character)</label>
+          <input type="password" placeholder="Password" class="form-control" name="password">
+        </div>
+        <div class="mb-2">
+          <label htmlFor="confirm_password">Confirmation password (same as password)</label>
+          <input type="password" placeholder="Confirmation Password" class="form-control" name="confirm_password">
+        </div>
+        <div class="d-grid mt-2">
+          <button class="btn btn-primary">Sign Up</button>
+        </div>
+        <p class="text-end mt-2">
+          Already Registered? <a href="login.php" class="ms-2">Sign In</a>
+        </p>
+      </form>
+    </div>
+  </div>
+  <script href="dist/js/bootstrap.bundle.min.js"></script>
+
+  <!-- <h1>Register</h1>
 
   <form method="POST">
     Enter your name (1 - 20 character)<br>
@@ -56,7 +95,8 @@ session_start();
     <input type="submit" value="Register"><br><br>
   </form>
 
-  <a href="login.php">Click to login</a>
+  <a href="login.php">Click to login</a> -->
 
 </body>
+
 </html>
